7c2a26e3e8b61794f5393d56896cb1ada5b26a8e
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-05-10 12:22:34 -0700

    adding fill by mean function

--- scripts/data_cleaning_functions.py
+++ scripts/data_cleaning_functions.py
@@ -11,3 +11,14 @@ def fill_missing_by_mode(df, cols):
     for col in mod_fill_list:
         df[col] = df[col].fillna(df[col].mode()[0])
     return df
+
+
+def fill_missing_by_mean(df, cols):
+    mean_fill_list = []
+    for i in range(cols.shape[0]):
+        if(cols.iloc[i,3] == "float64"):
+            mean_fill_list.append(cols.iloc[i,0])
+    
+    for col in mean_fill_list:
+        df[col] = df[col].fillna(df[col].median())
+    return df

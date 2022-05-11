520a6734060f1c084b48bfc8b318a92626b6e6a3
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-05-11 10:43:39 -0700

    saving overall progress

--- scripts/data_cleaning_functions.py
+++ scripts/data_cleaning_functions.py
@@ -12,22 +12,29 @@ class DataCleaner:
         self.summar = DataSummarizer() 
 
     def removeOutliers(self, df,cols):
+        df_temp = df.copy(deep=True)
         for col in cols:
             ''' Detection '''
             # IQR
-            Q1 = df[col].quantile(0.25)
+            Q1 = df_temp[col].quantile(0.25)
 
-            Q3 = df[col].quantile(0.75)
+            Q3 = df_temp[col].quantile(0.75)
             IQR = Q3 - Q1
+            rm_lis = []
+            length=df_temp.shape[0]
+            for index in range(length):
+                if(df_temp.loc[index,col] >= (Q3+1.5*IQR)):
+                    rm_lis.append(index)
 
-            # Upper bound
-            upper = np.where(df[col] >= (Q3+1.5*IQR))
-            # Lower bound
-            lower = np.where(df[col] <= (Q1-1.5*IQR))
+            # # Upper bound
+            # upper = np.where(df[col] >= (Q3+1.5*IQR))
+            # # Lower bound
+            # lower = np.where(df[col] <= (Q1-1.5*IQR))
 
             ''' Removing the Outliers '''
-            df.drop(upper[0], inplace = True)
-            df.drop(lower[0], inplace = True)
+            df_temp.drop(rm_lis, inplace = True)
+
+            return df_temp
     
     def remove_cols(self, df, cols, keep=False):
         """
